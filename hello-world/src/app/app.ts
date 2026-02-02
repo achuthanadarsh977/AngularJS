d17e860cf9799a3c9687eea19e868890f1fc93c0
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2026-02-02 19:02:26 +0530

    Add files via upload

---
+++ hello-world/src/app/app.ts
@@ -0,0 +1,13 @@
+// Main Angular App Component - hooks tested
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('hello-everyone');
+}
